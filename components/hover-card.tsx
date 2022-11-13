877a9ed86bfd5d52cfb891a2ff287671b15fbcfc
Alex Shaw <alexgshaw64@gmail.com>
2022-11-12 23:23:08 -0700

    Update navbar on mobile.

--- components/hover-card.tsx
+++ components/hover-card.tsx
@@ -14,7 +14,7 @@ export default function HoverCard({
 }: HoverCardProps) {
   return (
     <Component className="group relative flex" {...props}>
-      <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 rounded-2xl bg-zinc-50 opacity-0 transition duration-200 group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6" />
+      <div className="absolute -inset-y-6 -inset-x-4 z-0 hidden scale-95 rounded-2xl bg-zinc-50 opacity-0 transition duration-200 group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:block" />
       <div className={className}>{children}</div>
     </Component>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { ReactElement } from "react";
 import { joinClassNames } from "../lib/utils";
+import { Menu } from "@headlessui/react";
 
 interface NavbarItem {
   name: string;
@@ -19,8 +20,8 @@ const NAVBAR_ITEMS: NavbarItem[] = [
 export default function Navbar(): ReactElement {
   const router = useRouter();
   return (
-    <div className="sticky top-6 z-20 flex justify-center">
-      <div className="flex rounded-full bg-white px-4 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5">
+    <div className="sticky top-6 z-20 flex justify-end sm:justify-center">
+      <div className="hidden rounded-full bg-white px-4 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 sm:flex">
         {NAVBAR_ITEMS.map((item) => (
           <Link href={item.href} key={item.name}>
             <div
@@ -34,6 +35,45 @@ export default function Navbar(): ReactElement {
           </Link>
         ))}
       </div>
+      <Menu>
+        <Menu.Button className="rounded-full bg-white p-4 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </Menu.Button>
+        <Menu.Items className="absolute inset-x-0 top-16 rounded-2xl bg-white shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5">
+          {NAVBAR_ITEMS.map((item) => (
+            <Menu.Item key={item.name}>
+              {({ active }) => (
+                <Link href={item.href}>
+                  <div
+                    className={joinClassNames(
+                      "px-4 py-3 text-sm transition duration-200 hover:text-teal-500",
+                      active ? "bg-zinc-100" : "",
+                      router.pathname == item.href
+                        ? "text-teal-500"
+                        : "text-zinc-800"
+                    )}
+                  >
+                    {item.name}
+                  </div>
+                </Link>
+              )}
+            </Menu.Item>
+          ))}
+        </Menu.Items>
+      </Menu>
     </div>
   );
 }
